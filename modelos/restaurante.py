3d4bd8858887e8ca5d69931e73857b9921111946
08718424759 <fernando.ribeiro@uerj.br>
2024-10-22 17:52:19 -0300

    Criação da classe Restaurante

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,19 +1,27 @@
 class Restaurante:
-    nome = ''
-    categoria =''
-    ativo = False
+    restaurantes = []
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
+    def __init__(self, nome, categoria, ativo=False):
+        self.nome = nome
+        self.categoria = categoria
+        self._ativo = ativo
+        Restaurante.restaurantes.append(self)
+    
+    def __str__(self):
+        return f'{self.nome} | {self.categoria} | {self._ativo}'
+    
+    def listar_restaurantes():
+        print(f'{"Nome do restaurante".ljust(25)} | {"Categoria".ljust(20)} | {"Ativo".ljust(6)}')
+        print(f'{"-" * 57}')
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(20)} | {restaurante.ativo}')
+    
+    @property
+    def ativo(self):
+        return '☑' if self._ativo else '☐'
 
-restaurante_pizza = Restaurante()
-restaurante_pizza.nome = 'Pizza'
-restaurante_pizza.categoria = 'Italiana'
-restaurante_pizza.ativo = True
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_pizza = Restaurante('pizza Express','Italiana',True)
 
-restaurantes = [restaurante_praca, restaurante_pizza]
+Restaurante.listar_restaurantes()
 
-for restaurante in restaurantes:
-    if restaurante.ativo:
-        print(vars(restaurante))
